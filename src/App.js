c9dd0fee404ef367355d5bd0720a7110b49b0051
jackiesafari <107513244+jackiesafari@users.noreply.github.com>
2022-09-29 01:01:40 -0400

    Call the program from the dapp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 //IMPORTS
+import idl from './idl.json';
 import React, { useEffect, useState } from "react";
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 import { Program, Provider, web3 } from "@project-serum/anchor";
 import toast, { Toaster } from "react-hot-toast";
 import "./App.css";
+import kp from './keypair.json'
 
 //CONSTANTS
 
-const TEST_GIFS = [
-  "https://media.giphy.com/media/35LCBkf6buF9AuzOL7/giphy.gif",
-  "https://media.giphy.com/media/pxuSx9i61E40xaAFyF/giphy.gif",
-  "https://media.giphy.com/media/26gspNQegsL4F1Sqk/giphy.gif",
-];
+const { SystemProgram, Keypair } = web3;
+const arr = Object.values(kp._keypair.secretKey)
+const secret = new Uint8Array(arr)
+const baseAccount = web3.Keypair.fromSecretKey(secret)
+const programID = new PublicKey('C3HYPspumM3qzGVsdqBU1GFSEeFzdKaxYQWcVa4o7Rwe');
+const network = clusterApiUrl('devnet');
+const opts = {
+  preflightCommitment: "processed"
+}
+
 
 const App = () => {
   //useSTATE
@@ -21,162 +28,227 @@ const App = () => {
 
   //TOASTS
 
-  const showPhantomToast = () =>
-    toast("To sign in, download a Phantom Wallet 👻 at https://phantom.app");
-  const showConnectedWalletToast = () => toast.success("You're signed in!");
-  const showDisconnectedWalletToast = () => toast.success("You've signed out!");
-  const showGifSentToast = () => toast.success("GIF Sent!");
+   const showPhantomToast = () =>
+   toast("To sign in, download a Phantom Wallet 👻 at https://phantom.app");
+ const showConnectedWalletToast = () => toast.success("You're signed in!");
+ const showDisconnectedWalletToast = () => toast.success("You've signed out!");
+ const showGifSentToast = () => toast.success("GIF Sent!");
 
-  //ACTIONS
+ //ACTIONS
 
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { solana } = window;
+ const checkIfWalletIsConnected = async () => {
+   try {
+     const { solana } = window;
 
-      if (solana) {
-        if (solana.isPhantom) {
-          console.log("Phantom wallet found!");
+     if (solana) {
+       if (solana.isPhantom) {
+         console.log("Phantom wallet found!");
 
-          const response = await solana.connect({ onlyIfTrusted: true });
-          console.log(
-            "Connected with Public Key:",
-            response.publicKey.toString()
-          );
-          setWalletAddress(response.publicKey.toString());
-        }
-      } else {
-        showPhantomToast();
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+         const response = await solana.connect({ onlyIfTrusted: true });
+         console.log(
+           "Connected with Public Key:",
+           response.publicKey.toString()
+         );
+         setWalletAddress(response.publicKey.toString());
+       }
+     } else {
+       showPhantomToast();
+     }
+   } catch (error) {
+     console.error(error);
+   }
+ };
 
-  const connectWallet = async () => {
-    const { solana } = window;
+ const connectWallet = async () => {
+   const { solana } = window;
 
-    if (solana) {
-      const response = await solana.connect();
-      console.log("Connected with Public Key:", response.publicKey.toString());
-      setWalletAddress(response.publicKey.toString());
-      showConnectedWalletToast();
-    }
-  };
+   if (solana) {
+     const response = await solana.connect();
+     console.log("Connected with Public Key:", response.publicKey.toString());
+     setWalletAddress(response.publicKey.toString());
+     showConnectedWalletToast();
+   }
+ };
 
-  const disconnectWallet = () => {
-    console.log("Wallet Disconnected");
-    setWalletAddress(null);
-    showDisconnectedWalletToast();
-  };
+ const disconnectWallet = () => {
+   console.log("Wallet Disconnected");
+   setWalletAddress(null);
+   showDisconnectedWalletToast();
+ };
 
-  const onInputChange = (event) => {
-    const { value } = event.target;
-    setInputValue(value);
-  };
+ const onInputChange = (event) => {
+   const { value } = event.target;
+   setInputValue(value);
+ };
 
-  const sendGif = async () => {
-    if (inputValue.length > 0) {
-      console.log("Gif link:", inputValue);
-      setGifList([...gifList, inputValue]);
-      setInputValue("");
-      showGifSentToast();
-    } else {
-      console.log("Empty input. Try again.");
-    }
-  };
+ const getProgram = async () => {
+   const idl = await Program.fetchIdl(programID, getProvider());
+   return new Program(idl, programID, getProvider());
+ };
 
-  const renderNotConnectedContainer = () => (
-    <div className="container">
-      <button
-        className="cta-button connect-wallet-button"
-        onClick={connectWallet}
-      >
-        SIGN IN
-      </button>
-      <p className="header">Scene Portal</p>
-      <p className="sub-header">Your favorite scenes, on the blockchain</p>
-      <div className="moon" />
-      <div className="kiki" />
-    </div>
-  );
+ const getGifList = async () => {
+   try {
+     const program = await getProgram();
+     const account = await program.account.baseAccount.fetch(
+       baseAccount.publicKey
+     );
 
-  const renderConnectedContainer = () => (
-    <div className="connected-container">
-      <p className="connected-header">SCENE PORTAL</p>
-      <button
-        className="cta-button disconnect-wallet-button"
-        onClick={disconnectWallet}
-      >
-        SIGN OUT
-      </button>
-      <form
-        className="form"
-        onSubmit={(event) => {
-          event.preventDefault();
-          sendGif();
-        }}
-      >
-        <input
-          type="text"
-          placeholder="post your favorite film/tv scene"
-          value={inputValue}
-          onChange={onInputChange}
-        />
-        <button type="submit" className="cta-button submit-gif-button">
-          Submit
-        </button>
-      </form>
-      <div className="gif-grid">
-        {gifList.map((gif) => (
-          <div className="gif-item" key={gif}>
-            <img className="gif-image" src={gif} alt={gif} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+     console.log("Got the account", account);
+     setGifList(account.gifList);
+   } catch (error) {
+     console.log("Error in getGifList: ", error);
+     setGifList(null);
+   }
+ };
 
-  //useEFFECTS
+ const getProvider = () => {
+   const connection = new Connection(network, opts.preflightCommitment);
+   const provider = new Provider(
+     connection,
+     window.solana,
+     opts.preflightCommitment
+   );
+   return provider;
+ };
 
-  useEffect(() => {
-    const onLoad = async () => {
-      await checkIfWalletIsConnected();
-    };
-    window.addEventListener("load", onLoad);
-    return () => window.removeEventListener("load", onLoad);
-  }, []);
+ const createGifAccount = async () => {
+   try {
+     const provider = getProvider();
+     const program = await getProgram();
+     
+     console.log("ping")
+     await program.rpc.startStuffOff({
+       accounts: {
+         baseAccount: baseAccount.publicKey,
+         user: provider.wallet.publicKey,
+         systemProgram: SystemProgram.programId,
+       },
+       signers: [baseAccount]
+     });
+     console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
+     await getGifList();
+ 
+   } catch(error) {
+     console.log("Error creating BaseAccount account:", error)
+   }
+ }
 
-  useEffect(() => {
-    if (walletAddress) {
-      console.log("Fetching GIF list...");
+ const sendGif = async () => {
+   if (inputValue.length > 0) {
+     console.log("Gif link:", inputValue);
+     setGifList([...gifList, inputValue]);
+     setInputValue("");
+     showGifSentToast();
+   } else {
+     console.log("Empty input. Try again.");
+   }
+ };
 
-      // Call Solana program here.
+ const renderNotConnectedContainer = () => (
+   <div className="container">
+     <button
+       className="cta-button connect-wallet-button"
+       onClick={connectWallet}
+     >
+       SIGN IN
+     </button>
+     <p className="header">Scene Portal</p>
+     <p className="sub-header">Your favorite scenes, on the blockchain</p>
+     <div className="moon" />
+     <div className="kiki" />
+   </div>
+ );
 
-      setGifList(TEST_GIFS);
-    }
-  }, [walletAddress]);
+ const renderConnectedContainer = () => {
+   // If we hit this, it means the program account hasn't been initialized.
+   if (gifList === null) {
+     return (
+       <div className="connected-container">
+         <button className="cta-button submit-gif-button" onClick={createGifAccount}>
+           Do One-Time Initialization For GIF Program Account
+         </button>
+       </div>
+     );
+   }
+   // Otherwise, we're good! Account exists. User can submit GIFs.
+   else {
+     return (
+       <div className="connected-container">
+         <p className="connected-header">SCENE PORTAL</p>
+         <button
+           className="cta-button disconnect-wallet-button"
+           onClick={disconnectWallet}
+         >
+           SIGN OUT
+         </button>
+         <form
+           className="form"
+           onSubmit={(event) => {
+             event.preventDefault();
+             sendGif();
+           }}
+         >
+           <input
+             type="text"
+             placeholder="post your favorite film/tv scene"
+             value={inputValue}
+             onChange={onInputChange}
+           />
+           <button type="submit" className="cta-button submit-gif-button">
+             Submit
+           </button>
+         </form>
+         <div className="gif-grid">
+           {/* We use index as the key instead, also, the src is now item.gifLink */}
+           {gifList.map((item, index) => (
+             <div className="gif-item" key={index}>
+               <img className="gif-image" src={item.gifLink}  alt ={item.gifLink}/>
+             </div>
+           ))}
+         </div>
+       </div>
+     );
+   }
+ };
 
-  return (
-    <div className="App">
-      <div className={walletAddress ? "authed-container" : "container"}>
-        <Toaster
-          toastOptions={{
-            className: "",
-            duration: 3000,
-            style: {
-              border: "1px solid #713200",
-              padding: "16px",
-              color: "#713200",
-            },
-          }}
-        />
-        <div className="header-container">
-          {!walletAddress && renderNotConnectedContainer()}
-          {walletAddress && renderConnectedContainer()}
-        </div>
-      </div>
-    </div>
-  );
+ //useEFFECTS
+
+ useEffect(() => {
+   const onLoad = async () => {
+     await checkIfWalletIsConnected();
+   };
+   window.addEventListener("load", onLoad);
+   return () => window.removeEventListener("load", onLoad);
+ }, []);
+
+ useEffect(() => {
+   if (walletAddress) {
+     console.log("Fetching GIF list...");
+     getGifList();
+   }
+ }, [walletAddress]);
+
+ return (
+   <div className="App">
+     <div className={walletAddress ? "authed-container" : "container"}>
+       <Toaster
+         toastOptions={{
+           className: "",
+           duration: 3000,
+           style: {
+             border: "1px solid #713200",
+             padding: "16px",
+             color: "#713200",
+           },
+         }}
+       />
+       <div className="header-container">
+         {!walletAddress && renderNotConnectedContainer()}
+         {walletAddress && renderConnectedContainer()}
+       </div>
+     </div>
+   </div>
+ );
 };
 
 export default App;
